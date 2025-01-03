4cb029c6502a4d56ca9bd10e126f9e73895c8cdb
Julien Zhang <jzhang52@stuy.edu>
2025-01-02 20:45:30 -0500

    testing server

--- pipe_networking.c
+++ pipe_networking.c
@@ -53,15 +53,11 @@ int server_handshake(int *to_client) {
   char synAck[256];
   sprintf(synAck, "%d", ran);
   write(*to_client,synAck,strlen(synAck)+1);
+  printf("Server writing %d to client\n", ran);
   char temp[256];
   read(from_client, temp, sizeof(temp));
   int msg = atoi(temp);
-  if (msg - 1 == ran){
-    printf("Handshake established\n");
-  }
-  else{
-    printf("Handshake failed\n");
-  }
+  printf("Server reading %d from client\n", msg);
   return from_client;
 }
 
@@ -104,8 +100,10 @@ int client_handshake(int *to_server) {
   char temp[256];
   read(from_server,temp,sizeof(temp));
   int msg = atoi(temp);
+  printf("Client reading %d from server\n", msg);
   int updatedMsg = msg+1;
   sprintf(temp, "%d", updatedMsg);
+  printf("Client sending %s to server\n", temp);
   write(*to_server, temp, strlen(temp)+1);
   return from_server;
 }
